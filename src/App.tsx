f7ffac2dc1713c318e2a8ee24ff55971450d55ab
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-03-16 02:48:55 -0300

    bug in @ import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from './components/ui/button';
+// import { Button } from '@/components/ui/button';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline bg-slate-400">
       Hello world!
+      <Button>Olá mundo</Button>
     </h1>
   )
 }
